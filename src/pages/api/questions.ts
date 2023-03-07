be17197e3709dd9ba5893ac705b569e1b0f7b40b
Filip Kober <koberfilip2@gmail.com>
2023-03-07 16:03:27 +0100

    added cors for all

--- src/pages/api/questions.ts
+++ src/pages/api/questions.ts
@@ -1,12 +1,19 @@
 import { type NextApiRequest, type NextApiResponse } from 'next'
 import { PrismaClient } from '@prisma/client'
+import NextCors from 'nextjs-cors'
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+    
+    await NextCors(req, res, {
+        methods: ['GET'],
+        origin: '*',
+        optionsSuccessStatus: 200,
+    })
+
     if(req.method === 'GET') {
         const prisma = new PrismaClient()
         const pytania = await prisma.pytania.findMany();
         prisma.$disconnect().catch((e) => console.error(e))
-        res.setHeader('Access-Control-Allow-Origin', '*')
         res.status(200).json(pytania.sort(() => Math.random() - 0.5))
     }
 }
